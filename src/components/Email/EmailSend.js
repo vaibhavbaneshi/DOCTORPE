e1739d84450e06e0d65deb07c9d6e88c62a14eb6
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-03-27 15:41:34 +0530

    fiexed email templates

--- src/components/Email/EmailSend.js
+++ src/components/Email/EmailSend.js
@@ -1,25 +1,22 @@
 import emailjs from '@emailjs/browser';
 
 export async function sendDoctor (email, username, callIdString) {
-    const message = `Greetings Dr. ${username}! A patient wants to consult to you virtually. The Call Id is: ${callIdString} and the link to the meeting is: https://online-meet-rosy.vercel.app/`;
-
-    console.log(email, username, callIdString);
+    const message = `Greetings Dr. ${username}! A patient wants to consult to you virtually. The link to the meeting is: https://online-meet-rosy.vercel.app/ and the Call Id is: `;
 
     const para = {
       to_name: username,
       from_name: "DoctorPe Team",
       email: email,
       message: message,
+      link: callIdString
     };
     
-    const serviceID = "service_zqj7cp8";
-    const tempID = "template_lmuyngf";
-
-    console.log('hi there again');
+    const serviceID = "service_egvis7l"
+    const tempID="template_7h2lkff"
   
     await emailjs
       .send(serviceID, tempID, para,{
-        publicKey: 'DFenNDhLvNQ3XzPVC'
+        publicKey: '7B2itpD6d0z4vkdgv'
       })
       .then((res) => {
         console.log('response: ', res);
@@ -30,25 +27,25 @@ export async function sendDoctor (email, username, callIdString) {
 }
 
 export function sendPatient(email, username, callIdString) {
-    const message = `Greetings ${username}! your online meeting Call Id is: <strong><u>${callIdString}</u></strong>`;
+    const message = `Greetings ${username}! your online meeting Call Id is: `;
 
     const messageOptions = {
       to_name: username,
       from_name: "DoctorPe Team",
       email: email,
       message: message,
+      link: callIdString
     };
   
-    const serviceID = "service_zqj7cp8";
-    const tempID = "template_lmuyngf";
+    const serviceID = "service_egvis7l"
+    const tempID="template_7h2lkff"
   
     emailjs
       .send(serviceID, tempID, messageOptions,{
-        publicKey: 'DFenNDhLvNQ3XzPVC'
+        publicKey: '7B2itpD6d0z4vkdgv'
       })
       .then((res) => {
         console.log(res);
-        console.log("hi there");
       })
       .catch((err) => {
         console.log(err);
