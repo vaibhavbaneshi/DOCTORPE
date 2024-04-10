c3b30a579794a62f3d46b2f1f54b4313fe1feba4
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-04-10 10:46:56 +0530

    Updated the Facility Card and css

--- src/components/Home/FacilityCard.jsx
+++ src/components/Home/FacilityCard.jsx
@@ -3,11 +3,11 @@ import './FacilityCard.css'
 
 export const FacilityCard = ({ image, background, title, sub, onClick }) => {
 
-    return <div onClick={onClick} className="facility-card-box hover:shadow-2xl hover:shadow-sky-300">
+    return <div onClick={onClick} className="facility-card-box flex flex-col items-center justify-center hover:shadow-2xl hover:shadow-cyan-300 w-60 h-80">
         <div className="facility-card-image-container " style={{ backgroundColor: `${background}` }}>
             <img src={image} alt={title} />
         </div>
-        <div className="facility-card-info">
+        <div className="facility-card-info text-center">
             <p>{title}</p>
             <p>{sub}</p>
         </div>
