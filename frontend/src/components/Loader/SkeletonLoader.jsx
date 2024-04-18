14725bf4a9f681f81c7d36b42d2bf84793ca4592
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-04-18 20:44:19 +0530

    edited the skeleton

--- frontend/src/components/Loader/SkeletonLoader.jsx
+++ frontend/src/components/Loader/SkeletonLoader.jsx
@@ -3,7 +3,7 @@ import './SkeletonLoader.css'
 
 export const SkeletonLoader = () => {
   return (
-    <div className="container p-10">
+    <div className="container">
       <div className="col-sm-6 col-md-3">
         <div className="movie--isloading">
           <div className="loading-image"></div>
