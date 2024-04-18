2020080fe6923067989fc62c5ad026a5e3e063f0
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-04-18 11:02:59 +0530

    added countdown to the Succes Product Purchase Page

--- frontend/src/pages/productPurchase/SuccessPurchase.jsx
+++ frontend/src/pages/productPurchase/SuccessPurchase.jsx
@@ -10,6 +10,8 @@ export const SuccessPurchase = ({ itemTotal, itemAmount }) => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const params = useParams()
+    const [ count, setCount ] = useState(7);
+    const [showAlert, setShowAlert] = useState(true);
 
     const loggedInPatientEmail = currentUser.data.email;
     const loggedInPatientfullname = currentUser.data.firstName + ' ' + currentUser.data.lastName;
@@ -20,11 +22,39 @@ export const SuccessPurchase = ({ itemTotal, itemAmount }) => {
         sendDelivery(loggedInPatientfullname, loggedInPatientEmail, totalItems, totalAmount);
     }, [])
 
-    setTimeout(() => {
-        navigate('/shoppingCart')
-    }, 5000)
+    useEffect(() => {
+        let timeoutId;
+    
+        const countdown = () => {
+            setCount(prevCount => prevCount - 1);
+            if (count === 1) {
+                clearTimeout(timeoutId);
+                navigate('/shoppingCart')
+            }
+        };
+    
+        if (showAlert) {
+            timeoutId = setInterval(countdown, 1000);
+        }
+    
+        return () => clearInterval(timeoutId);
+    }, [showAlert, count]);
     
     return <>
-        {<SuccessMessage message={`Order placed successfully and order details have been sent to your email : ${currentUser.data.email}`} />}
+        {showAlert && (
+                <div className="ml-96 bg-green-200 border border-green-400 text-green-700 px-4 py-3 rounded mt-4 sticky top-0 z-10 inline-block">
+                    <div className="flex items-center">
+                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 mr-2">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                        </svg>
+                        <p className="font-bold pr-3">Success alert!</p>
+                        <p>Order placed successfully and order details have been sent to your email : ${currentUser.data.email}</p>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <p className="">Redirecting in : {count}</p>
+                    </div>
+                </div>
+            )}
     </>
 }
