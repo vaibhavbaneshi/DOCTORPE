eb27aa007251c07364537f9aa5e3ff575a8bb3d3
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-04-18 12:55:31 +0530

    Updated the Input Box

--- frontend/src/components/Form/InputBox.jsx
+++ frontend/src/components/Form/InputBox.jsx
@@ -3,6 +3,6 @@ export const InputBox = ({ label,type,name,id, placeholder, onChange,className,v
     return <div className="md:col-span-5 mb-4">
     <label htmlFor={name}>{label}</label>
     {!error && <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} className={`h-10 border mt-1 rounded px-4 w-full bg-gray-50 ${className}`} value={value}  />}
-    {error && <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} className={`h-10 border border-red mt-1 rounded px-4 w-full bg-gray-50 ${className}`} value={value}  />}
+    {error && <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} className={`h-10 border border-rose-500 mt-1 rounded px-4 w-full bg-gray-50 ${className}`} value={value}  />}
   </div>
 }
