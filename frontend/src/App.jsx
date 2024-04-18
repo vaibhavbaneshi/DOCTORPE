1034e4d4f145d0686026a9f845c1a9108a5c5eb5
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-04-18 20:05:13 +0530

    Removed the Video Call Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { HomePage } from "./pages/home/HomePage"
 import { Navigation } from "./components/Navbar/Navigation"
 import Signin from "./pages/auth/Signin"
 import Signup from "./pages/auth/Signup"
 import { SearchDoctor } from "./pages/searchDoctor/SearchDoctor"
-import { VideoCall } from "./components/VideoCall/VideoCall"
 import AiLab from "./pages/AiLab/AiLab"
 import { BookDoctor } from "./pages/searchDoctor/BookDoctor"
 import { ShoppingCart } from "./pages/ShoppingCart/ShoppingCart"
@@ -27,7 +26,6 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/searchDoctor" element={<SearchDoctor />} />
           <Route path="/bookDoctor" element={<BookDoctor />} />
-          <Route path="/consult" element={<VideoCall />} />
           <Route path="/medicines"element={<ProductsScreen />}/>
           <Route path="/medicines/:title" element={<ProductDetail />} />
           <Route path="/AI_Lab"element={<AiLab />}/>
