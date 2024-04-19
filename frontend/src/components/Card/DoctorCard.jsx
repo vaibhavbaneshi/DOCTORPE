c4178c212316482182b1945a6b7f6efc1444cb6d
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-04-19 13:40:34 +0530

    Edited padding in the Doctor Card

--- frontend/src/components/Card/DoctorCard.jsx
+++ frontend/src/components/Card/DoctorCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const DoctorCard = ({ onClick, name, description, speciality, label }) => {
 
     return (
-        <div className="flex items-center mt-20 mb-20 justify-center transition duration-700 ease-in-out transform hover:scale-105">
+        <div className="px-6 py-2 flex items-center mt-20 mb-20 justify-center transition duration-700 ease-in-out transform hover:scale-105">
             <div className="w-60 h-80 bg-white rounded-3xl  text-neutral-300 p-4 flex flex-col items-center justify-center gap-3 hover:shadow-2xl hover:shadow-cyan-400 transition-shadow">
                 <div className="w-32 h-32  "><img src="images/docvid.png" alt="doctor image" /></div>
                 <div className="text-black text-center">
