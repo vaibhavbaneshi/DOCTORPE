27a196375bebec51c8dbb6577fae0e5241ecb21f
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-05-05 23:01:25 +0530

    Fixed the Handle Location Bug

--- frontend/src/components/DoctorLocation/DoctorLocation.jsx
+++ frontend/src/components/DoctorLocation/DoctorLocation.jsx
@@ -43,13 +43,17 @@ export const DoctorLocation = ({ handleLocationClick }) => {
     const [searchLocation, setSearchLocation] = useState('');
     const [locations, setLocations] = useState(initLocations);
 
+    const handleLocationItemClick  = () => {
+        handleLocationClick()
+    }
+
     return <div className="home-search-container">
         <div className="location-search-box">
             <img src={'../../../images/home_location_icon.svg'} alt="" width="22" />
             <input type="text" className="search-location-input-box" placeholder="Search location" onFocus={() => setLocationResultHidden(false)} onBlur={() => setLocationResultHidden(true)} value={searchLocation} onChange={(e) => setSearchLocation(e.target.value)} />
             <div className="search-location-input-results" hidden={locationResultHidden}>
                 {
-                    locations.map(location => <div className="search-location-result-item" key={location.place} onClick={handleLocationClick} onMouseDown={() => setSearchLocation(location.place)}>
+                    locations.map(location => <div className="search-location-result-item" key={location.place} onClick={handleLocationItemClick} onMouseDown={() => setSearchLocation(location.place)}>
                         <span><img src={'../../../images/search.svg'} alt="" width="12" /></span>
                         <span><div>{location.place}</div><div>{location.city}</div></span>
                     </div>)
