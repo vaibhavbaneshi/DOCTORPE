10114300fb75ff47a3857c81106c7b53e6613372
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-05-06 12:44:29 +0530

    Added Location of Doctor

--- frontend/src/components/DoctorLocation/DoctorLocation.jsx
+++ frontend/src/components/DoctorLocation/DoctorLocation.jsx
@@ -43,22 +43,34 @@ export const DoctorLocation = ({ handleLocationSelect }) => {
     const [locations, setLocations] = useState(initLocations);
 
     return <div className="home-search-container">
-        <div className="location-search-box">
-            <img src={'../../../images/home_location_icon.svg'} alt="" width="22" />
-            <input type="text" className="search-location-input-box" placeholder="Search location" onFocus={() => setLocationResultHidden(false)} onBlur={() => setLocationResultHidden(true)} value={searchLocation} onChange={(e) => setSearchLocation(e.target.value)} />
-            <div className="search-location-input-results" hidden={locationResultHidden}>
-                {
-                    locations.map(location => <div className="search-location-result-item" key={location.place} onClick={handleLocationSelect} onMouseDown={() => setSearchLocation(location.place)}>
-                        <span>
-                            <img src={'../../../images/search.svg'} alt="" width="12" />
-                        </span>
-                        <span>
-                            <div>{location.place}</div>
-                            <div>{location.city}</div>
-                        </span>
-                    </div>)
-                }
+                <div className="location-search-box">
+                    <img className="rounded-md bg-slate-200 pr-1 border-black" src={'../../../images/home_location_icon.svg'} alt="" width="32" />
+                    <input type="text" className="search-location-input-box rounded-md" placeholder="Search location" onFocus={() => setLocationResultHidden(false)} onBlur={() => setLocationResultHidden(true)} value={searchLocation} onChange={(e) => setSearchLocation(e.target.value)} />
+                    <div className="search-location-input-results" hidden={locationResultHidden}>
+                        {
+                            locations.map(location => (
+                                <div className="search-location-result-item" key={location.place} onMouseDown={() => setSearchLocation(location.place)}>
+                                    <span>
+                                        <img src={'../../../images/search.svg'} alt="" width="20" />
+                                    </span>
+                                    <span>
+                                        <div>{location.place}</div>
+                                        <div>{location.city}</div>
+                                        <button 
+                                            onClick={() => {
+                                            console.log("hi there");
+                                            setShowLoader(true);
+                                            setTimeout(() => {
+                                                setShowLoader(false);
+                                            }, 5000);}}
+
+                                            className="hover:cursor-pointer rounded-lg px-2 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-400 hover:to-cyan-400 text-white"
+                                        >Select</button>
+                                    </span>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 }
